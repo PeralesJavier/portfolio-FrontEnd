561d2a0349013b4cd6dc9377b76e83a8f50b907d
Javier <javier.perales175962@alumnos.info.unlp.edu.ar>
2022-08-20 17:53:29 -0300

    Agregando bootstrap a componentes del header

--- portfolio_JavierPerales/src/app/app.module.ts
+++ portfolio_JavierPerales/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoArgProgComponent } from './components/logo-arg-prog/logo-arg-prog.component';
-import { SocialNetComponent } from './components/social-net/social-net.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeMiComponent } from './components/acerca-de-mi/acerca-de-mi.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
@@ -19,7 +18,6 @@ import { FooterComponent } from './components/footer/footer.component';
     AppComponent,
     HeaderComponent,
     LogoArgProgComponent,
-    SocialNetComponent,
     BannerComponent,
     AcercaDeMiComponent,
     ExperienciaComponent,

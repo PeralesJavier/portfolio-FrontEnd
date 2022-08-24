a0efdb36d368a70fbfbc9c285797c357fe953554
Javier <javier.perales175962@alumnos.info.unlp.edu.ar>
2022-08-23 23:40:55 -0300

    Agregando el routing de los componentes login y home

--- portfolio_JavierPerales/src/app/app.module.ts
+++ portfolio_JavierPerales/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoArgProgComponent } from './components/logo-arg-prog/logo-arg-prog.component';
@@ -12,7 +12,9 @@ import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HardSoftSkillsComponent } from './components/hard-soft-skills/hard-soft-skills.component';
 import { ProyectosComponent } from './components/proyectos/proyectos.component';
 import { FooterComponent } from './components/footer/footer.component';
-import{ HttpClientModule } from '@angular/common/http'
+import{ HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component'
 
 @NgModule({
   declarations: [
@@ -26,9 +28,12 @@ import{ HttpClientModule } from '@angular/common/http'
     HardSoftSkillsComponent,
     ProyectosComponent,
     FooterComponent,
+    HomeComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     NgCircleProgressModule.forRoot({}),
     HttpClientModule
   ],
--- portfolio_JavierPerales/src/app/components/logo-arg-prog/logo-arg-prog.component.ts
+++ portfolio_JavierPerales/src/app/components/logo-arg-prog/logo-arg-prog.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-logo-arg-prog',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LogoArgProgComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
+  }
+
+  login(){
+    this.router.navigate(['/login'])
   }
 
 }
